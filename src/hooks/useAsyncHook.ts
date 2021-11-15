cae0dfe9e331ae06c1b4a39d309b5bd81b1e5ad9
victor.longon <vhlongon@gmail.com>
2021-11-15 23:48:59 +0100

    add useAsyncHook

---
+++ src/hooks/useAsyncHook.ts
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useState } from "react";
+import { Status } from "../types";
+
+export const useAsync = function <T = unknown>(
+  asyncFunction: (args?: unknown) => Promise<T>,
+  immediate = true
+) {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState<Status>("idle");
+
+  const execute = useCallback(() => {
+    setStatus("pending");
+    setData(null);
+    setError(null);
+    return asyncFunction()
+      .then((response) => {
+        setData(response);
+        setStatus("success");
+      })
+      .catch((error) => {
+        setError(error);
+        setStatus("error");
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+  }, [execute, immediate]);
+  return { execute, status, data, error };
+};
