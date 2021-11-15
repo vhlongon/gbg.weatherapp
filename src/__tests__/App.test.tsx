94090ccb8bbae548871b337d40025c2c84838404
victor.longon <vhlongon@gmail.com>
2021-11-15 23:50:05 +0100

    Implement app root and add tests

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import App from "../App";
+import * as api from "../api";
+import { ResponseData } from "../types";
+
+jest.mock("../api", () => ({ fetchWeatherData: jest.fn() }));
+
+let mockFetchWeatherData = jest.spyOn(api, "fetchWeatherData");
+
+describe("App", () => {
+  test("renders correctly when fetch data is successful", async () => {
+    mockFetchWeatherData.mockResolvedValueOnce([
+      {
+        lat: 123,
+        lon: 456,
+        current: {
+          dt: 1636925233,
+          sunrise: 1636872900,
+          sunset: 1636901898,
+          temp: 5.33,
+          feels_like: 2.5,
+          pressure: 1034,
+          humidity: 85,
+          dew_point: 3.02,
+          uvi: 0,
+          clouds: 90,
+          visibility: 10000,
+          wind_speed: 3.6,
+          wind_deg: 150,
+          weather: [
+            {
+              id: 804,
+              main: "Clouds",
+              description: "overcast clouds",
+              icon: "04n",
+            },
+          ],
+        },
+        hourly: [
+          {
+            dt: 1636848000,
+            temp: 6.81,
+            feels_like: 3.77,
+            pressure: 1023,
+            humidity: 92,
+            dew_point: 5.6,
+            uvi: 0,
+            clouds: 90,
+            visibility: 10000,
+            wind_speed: 4.63,
+            wind_deg: 30,
+            weather: [
+              {
+                id: 804,
+                main: "Clouds",
+                description: "overcast clouds",
+                icon: "04n",
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+
+    render(<App />);
+
+    await waitForElementToBeRemoved(() => screen.getByText("loading..."));
+
+    expect(screen.getByLabelText("weather-list")).toBeInTheDocument();
+  });
+
+  test("renders error when fetch data fails", async () => {
+    mockFetchWeatherData.mockRejectedValueOnce(new Error("oops"));
+
+    render(<App />);
+
+    await waitForElementToBeRemoved(() => screen.getByText("loading..."));
+
+    expect(screen.getByText("oops")).toBeInTheDocument();
+  });
+});
