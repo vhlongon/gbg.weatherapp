24baa1c7a20189048bcfd4038bbeeb412dc9c586
victor.longon <vhlongon@gmail.com>
2021-11-15 23:47:19 +0100

    setup project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import ViteFonts from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    ViteFonts({
+      google: {
+        families: [
+          { name: "Source Sans Pro", styles: "ital,wght@0,400;1,200;600" },
+        ],
+      },
+    }),
+  ],
 });
