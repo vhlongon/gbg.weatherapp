d6f302dee2e61a797590a3df8e0304adb3d6bf78
victor.longon <vhlongon@gmail.com>
2021-11-23 14:41:34 +0100

    refactor API and move const from function body

--- src/api.ts
+++ src/api.ts
@@ -5,10 +5,10 @@ const GBG_COORDS = {
   lat: 57.70887,
   lon: 11.97456,
 };
+const KEY = import.meta.env.VITE_WEATHER_APP_KEY;
+const URL = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${GBG_COORDS.lat}&lon=${GBG_COORDS.lon}&units=metric&appid=${KEY}`;
 
 export const fetchWeatherData = async (): Promise<ResponseData[]> => {
-  const KEY = import.meta.env.VITE_WEATHER_APP_KEY;
-  const URL = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${GBG_COORDS.lat}&lon=${GBG_COORDS.lon}&units=metric&appid=${KEY}`;
   const today = new Date();
   const days = Array.from({ length: 5 }, (_, index) =>
     Math.floor(subDays(today, index).getTime() / 1000)
